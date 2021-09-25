df98fcf19108188d165487ad1015bee2c75e2701
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-09-25 12:49:03 -0400

    Delete deneme.sh

--- deneme.sh
+++
@@ -1,5 +0,0 @@
-figlet FARUKTEK.DEVELOPER
-echo " _____________________________________________________ "
-echo "MERHABA HAWKSPLOİTİ ÇALIŞTIRMAYA VE İNDİRMEYE HAZIRMISIN :) "
-cd /usr/bin/
-wget https://raw.githubusercontent.com/hawk-unity/hawksploit/main/hackingtools.py
