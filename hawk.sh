2900e056d2714362ae0e15f1c29b8c5da353e721
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-09-25 13:02:31 -0400

    Create hawk.sh

---
+++ hawk.sh
@@ -0,0 +1,5 @@
+figlet FARUKTEK.DEVELOPER
+echo " _____________________________________________________ "
+echo "MERHABA HAWKSPLOİTİ ÇALIŞTIRMAYA VE İNDİRMEYE HAZIRMISIN :) "
+cd /usr/bin/
+wget https://raw.githubusercontent.com/hawk-unity/hawksploit/main/hawksploit
